62f9bda8db2ae898bc77528e93c107cd7850caec
Fergus McClean <fergus.mcclean@newcastle.ac.uk>
2021-11-26 12:15:01 +0000

    Remove data folder for inputs

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os
 from shutil import copyfile
 import subprocess
-subprocess.run(['python', '-m',  'openudm', '/data/inputs/data'])
+subprocess.run(['python', '-m',  'openudm', '/data/inputs'])
 
 # move files to output dir
-result_data_dir = '/data/inputs/data'
+result_data_dir = '/data/inputs'
 output_data_dir = '/data/outputs/data'
 buildings_data_dir = '/data/outputs/buildings'
 
@@ -12,7 +12,7 @@ buildings_data_dir = '/data/outputs/buildings'
 os.makedirs(output_data_dir, exist_ok=True)
 
 # copy the listed output files to the output location
-files_to_copy = ['out_cell_dev.asc','out_cell_dph.asc', 'out_cell_suit.asc']
+files_to_copy = ['out_cell_dev.asc', 'out_cell_dph.asc', 'out_cell_suit.asc']
 for file_name in files_to_copy:
     copyfile(os.path.join(result_data_dir, file_name), os.path.join(output_data_dir, file_name))
 
@@ -20,6 +20,6 @@ for file_name in files_to_copy:
 # make output dir if not exists
 os.makedirs(buildings_data_dir, exist_ok=True)
 urban_fabric_raster = os.path.join(output_data_dir, 'out_uf.asc')
-subprocess.run(['generate_urban_fabric', '-i', '/data/inputs/data/out_cell_dph.asc', '-o', urban_fabric_raster])
+subprocess.run(['generate_urban_fabric', '-i', '/data/inputs/out_cell_dph.asc', '-o', urban_fabric_raster])
 subprocess.run(['raster_to_vector', '-i', urban_fabric_raster, '-o',
                 os.path.join(buildings_data_dir, "urban_fabric.gpkg"), '-f' 'buildings'])
