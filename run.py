4a62c8fe0e1ddc17e347cf38d362085c84d7f07b
Fergus McClean <fergus.mcclean@newcastle.ac.uk>
2021-11-26 12:33:59 +0000

    Create metadata.json using TITLE parameter

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from shutil import copyfile
 import subprocess
+import datetime
 subprocess.run(['python', '-m',  'openudm', '/data/inputs'])
 
 # move files to output dir
@@ -23,3 +24,45 @@ urban_fabric_raster = os.path.join(output_data_dir, 'out_uf.asc')
 subprocess.run(['generate_urban_fabric', '-i', '/data/inputs/out_cell_dph.asc', '-o', urban_fabric_raster])
 subprocess.run(['raster_to_vector', '-i', urban_fabric_raster, '-o',
                 os.path.join(buildings_data_dir, "urban_fabric.gpkg"), '-f' 'buildings'])
+
+title = os.getenv('TITLE', 'OpenUDM output')
+description = ' '
+geojson = {}
+
+metadata = f"""{{
+  "@context": ["metadata-v1"],
+  "@type": "dcat:Dataset",
+  "dct:language": "en",
+  "dct:title": "{title}",
+  "dct:description": "{description}",
+  "dcat:keyword": [
+    "citycat"
+  ],
+  "dct:subject": "Environment",
+  "dct:license": {{
+    "@type": "LicenseDocument",
+    "@id": "https://creativecommons.org/licences/by/4.0/",
+    "rdfs:label": null
+  }},
+  "dct:creator": [{{"@type": "foaf:Organization"}}],
+  "dcat:contactPoint": {{
+    "@type": "vcard:Organization",
+    "vcard:fn": "DAFNI",
+    "vcard:hasEmail": "support@dafni.ac.uk"
+  }},
+  "dct:created": "{datetime.datetime.now().isoformat()}Z",
+  "dct:PeriodOfTime": {{
+    "type": "dct:PeriodOfTime",
+    "time:hasBeginning": null,
+    "time:hasEnd": null
+  }},
+  "dafni_version_note": "created",
+  "dct:spatial": {{
+    "@type": "dct:Location",
+    "rdfs:label": null
+  }},
+  "geojson": {geojson}
+}}
+"""
+with open(os.path.join('/data/outputs/metadata.json'), 'w') as f:
+    f.write(metadata)
