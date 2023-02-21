998711c4f2b0714e9819dfd898a11951421bba75
James Virgo <47320567+JamesVirgo@users.noreply.github.com>
2023-02-21 15:51:14 +0000

    zip udm outputs

--- run.py
+++ run.py
@@ -7,7 +7,9 @@ import pandas as pd
 
 from os import getenv, walk, mkdir, remove, listdir
 from os.path import join, isdir, isfile
+import zipfile
 
+import csv
 
 def zip_file(path, file):
     """
@@ -113,25 +115,57 @@ print(find_files())
 # move files to output dir
 
 # copy the listed output files to the output location
-files_to_copy = ['out_cell_dev.asc', 'out_cell_pph.asc', 'out_cell_dph.asc', 'out_cell_suit.asc', 'out_cell_overflow.csv']
-for file_name in files_to_copy:
+files_to_copy_1 = ['attractors.csv', 'constraints.csv', 'parameters.csv', 'population.csv']
+for file_name in files_to_copy_1:
+    copyfile(os.path.join(result_data_dir, file_name), os.path.join(output_data_dir, file_name))
+
+files_to_copy_2 = ['out_cell_dev.asc', 'out_cell_suit.asc', 'out_cell_pph.asc', 'out_cell_dph.asc']
+for file_name in files_to_copy_2:
+    copyfile(os.path.join(result_data_dir, file_name), os.path.join(output_data_dir, file_name))
+
+files_to_copy_3 = ['out_cell_density_band.asc', 'out_cell_build_type.asc', 'out_cell_tile_type.asc']
+for file_name in files_to_copy_3:
+    copyfile(os.path.join(result_data_dir, file_name), os.path.join(output_data_dir, file_name))
+
+files_to_copy_4 = ['out_cell_roads_cov.asc', 'out_cell_green_cov.asc', 'out_cell_build_cov.asc', 'out_cell_overflow.csv']
+for file_name in files_to_copy_4:
     copyfile(os.path.join(result_data_dir, file_name), os.path.join(output_data_dir, file_name))
 
 print('*** Moved UDM output files ***')
+
+# zip output dir
+
+#name = '/data/outputs/data'
+#zip_name = name + '.zip'
+
+#with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+#    for folder_name, subfolders, filenames in os.walk(name):
+#        for filename in filenames:
+#            file_path = os.path.join(folder_name, filename)
+#            zip_ref.write(file_path, arcname=os.path.relpath(file_path, name))
+
+#zip_ref.close()
+
+#print('*** Zipped UDM output files ***')
 
 # if we want to rename any file output by UDM do this here.....
 #
 #
 
-# run ufg tool is set by user
+# run ufg tool if set by user
 if run_ufg:
+    
     # run urban fabric generator tool
-    # make output dir if not exists
-    os.makedirs(buildings_data_dir, exist_ok=True)
+    build_type_ras = os.path.join(result_data_dir, 'out_cell_build_type.asc')
+    tile_type_ras = os.path.join(result_data_dir, 'out_cell_tile_type.asc')
     urban_fabric_raster = os.path.join(output_data_dir, 'out_uf.asc')
-    subprocess.run(['generate_urban_fabric', '-i', '/data/outputs/data/out_cell_dph.asc', '-o', urban_fabric_raster])
+
+    subprocess.run(['ufg_fabric', '-b', build_type_ras, '-t', tile_type_ras, '-f', urban_fabric_raster])    
 
     print('*** Ran UFG ***')
+
+    # make output dir if not exists
+    os.makedirs(buildings_data_dir, exist_ok=True)
 
     # run raster to vector tool
     subprocess.run(['raster_to_vector', '-i', '/data/outputs/data/out_uf.asc', '-o',
@@ -156,3 +190,37 @@ if run_ufg:
     zip_file(output_data_dir, 'out_uf.asc')
 
     print('*** Ran R2V ***')
+
+# zip output dir
+
+metadata_tbl = '/data/outputs/data/metadata.csv'
+ssp = ''
+year = ''  
+with open(metadata_tbl) as csvfile:
+    reader = csv.DictReader(csvfile)        
+    for row in reader:        
+        if row['PARAMETER'] == 'SSP':  
+            ssp = row['VALUE']          
+        elif row['PARAMETER'] == 'YEAR':
+            year = row['VALUE']
+print(ssp)  
+print(year)   
+
+z_name1 = 'ssp' + ssp + '-' + year 
+print(z_name1)
+
+print( "Metadata file imported.")
+
+name = '/data/outputs/data'
+z_name2 = '/data/outputs/' + z_name1
+zip_name = z_name2 + '.zip'
+
+with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+    for folder_name, subfolders, filenames in os.walk(name):
+        for filename in filenames:
+            file_path = os.path.join(folder_name, filename)
+            zip_ref.write(file_path, arcname=os.path.relpath(file_path, name))
+
+zip_ref.close()
+
+print('*** Zipped UDM output files ***')
